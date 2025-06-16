359e301cc863a77d2fd7c567d5db521add85dbbf
NiassDiouf <ibrahimaniassdiouf@gmail.com>
2025-06-16 00:58:06 +0000

    commentaire app listen

--- api/index.js
+++ api/index.js
@@ -19,9 +19,9 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 3000}`);
-    });
+    // app.listen(process.env.PORT || 3000, () => {
+    //   console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    // });
     })
     .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
