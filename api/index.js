a161458ef56a6f674fdb3895adb0c745675cb437
NiassDiouf <ibrahimaniassdiouf@gmail.com>
2025-06-20 04:18:36 +0000

    Optimisation

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 require('dotenv').config();
+const compression = require('compression');
 const mongoose = require("mongoose");
 const hotelRoute = require("./routes/hotel.route.js");
 const cors = require('cors');
 const app = express();
 
 // Middleware
+app.use(compression());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // Routes
@@ -19,9 +22,9 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    // app.listen(process.env.PORT || 3000, () => {
-    //   console.log(`Server is running on port ${process.env.PORT || 3000}`);
-    // });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
     })
     .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
