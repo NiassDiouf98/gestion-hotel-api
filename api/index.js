e665f01c7be5341a594afb2783f9e5ee605964ce
NiassDiouf <ibrahimaniassdiouf@gmail.com>
2025-06-20 05:20:47 +0000

    final optimisation

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const compression = require('compression');
 const mongoose = require("mongoose");
 const hotelRoute = require("./routes/hotel.route.js");
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 
 // Middleware
@@ -11,9 +14,21 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100 // Limite : 100 requêtes par IP toutes les 15 min
+}));
 
 // Routes
 app.use("/api/hotels", hotelRoute);
+
+// Gestion des erreurs
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Erreur serveur' });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, from Express.js!");
@@ -22,9 +37,9 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 3000}`);
-    });
+    // app.listen(process.env.PORT || 3000, () => {
+    //   console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    // });
     })
     .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
